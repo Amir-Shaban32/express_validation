b1e9dcf2aa4dc62829f539870a936e42cd2f773e
Amir-Shaban32 <https://github.com/Amir-Shaban32>
2025-07-31 15:24:30 +0300

    remove public

--- public/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authValidate_1 = require("../validators/authValidate");
-const authController_1 = require("../Controllers/authController");
-const router = (0, express_1.Router)();
-router.post('/', authValidate_1.registerValidate, authController_1.register);
-router.get('/test', (req, res) => res.send('Auth router works'));
-exports.default = router;
